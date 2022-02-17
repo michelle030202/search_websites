de9a6001c950fa2cd7a0316312a6cdcbacbb8116
Michelle berezin <michelle@h-MacBook-Pro-sl-Michelle.local>
2022-02-17 23:39:02 +0200

    first commit

---
+++ spider.py
@@ -0,0 +1,42 @@
+from urllib.request import urlopen
+from link finder import linkFinder
+from general import *
+
+class Spider:
+    # class variable (shared among all instance)
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('First spider', Spider.base_url)
+
+    @staticmethod
+    def boot(self):
+        create_project_directory(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' now crawling ' + page_url)
+            print(' Queue ' + str(len(Spider.queue)) + '| crawled ' + str(len(Spider.crawled)))
+
+
+
+
+
+
+
